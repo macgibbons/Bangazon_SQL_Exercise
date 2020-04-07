e155e5fb22dc4339dc4b4e9bdd60c8f0b2a55bcc
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-04-07 11:18:08 -0500

    created sql file, wrote, and commented out the questions

---
+++ Bangazon_SQL_Exercise.sql
@@ -0,0 +1,71 @@
+﻿---------- BANGAZON SQL EXERCISE ----------
+			 -- Mac Gibbons --
+			 -- 04/07/2020 --
+
+-- 1: List each employee first name, last name and supervisor status along with their department name. Order by department name, then by employee last name, and finally by employee first name.
+
+
+-- 2: List each department ordered by budget amount with the highest first.
+
+
+-- 3: List each department name along with any employees (full name) in that department who are supervisors.
+
+
+-- 4: List each department name along with a count of employees in each department.
+
+
+-- 5: Write a single update statement to increase each department's budget by 20%.
+
+
+-- 6: List the employee full names for employees who are not signed up for any training programs.
+
+
+-- 7: List the employee full names for employees who are signed up for at least one training program and include the number of training programs they are signed up for.
+
+
+-- 8: List all training programs along with the count employees who have signed up for each.
+
+
+-- 9: List all training programs who have no more seats available.
+
+
+-- 10: List all future training programs ordered by start date with the earliest date first.
+
+
+-- 11: Assign a few employees to training programs of your choice.
+
+
+-- 12: List the top 3 most popular training programs. (For this question, consider each record in the training program table to be a UNIQUE training program).
+
+
+-- 13: List the top 3 most popular training programs. (For this question consider training programs with the same name to be the SAME training program).
+
+
+-- 14: List all employees who do not have computers.
+
+
+-- 15: List all employees along with their current computer information make and manufacturer combined into a field entitled ComputerInfo. If they do not have a computer, this field should say "N/A".
+
+
+-- 16: List all computers that were purchased before July 2019 that are have not been decommissioned.
+
+
+-- 17: List all employees along with the total number of computers they have ever had.
+
+
+-- 18: List the number of customers using each payment type
+
+
+-- 19: List the 10 most expensive products and the names of the seller
+
+
+-- 20: List the 10 most purchased products and the names of the seller
+
+
+-- 21: Find the name of the customer who has made the most purchases
+
+
+-- 22: List the amount of total sales by product type
+
+
+-- 23: List the total amount made from all sellers
